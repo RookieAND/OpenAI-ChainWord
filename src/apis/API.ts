afcbf968e18db46b8f17a042ef40dff5e58dfbd3
RookieAND <gwangin1999@naver.com>
2023-02-18 22:39:41 +0900

    feat: openAI 요청 질의 영문으로 수정

--- src/apis/API.ts
+++ src/apis/API.ts
@@ -1,9 +1,16 @@
 import { Configuration, OpenAIApi } from 'openai';
 import { VITE_OPENAI_API_KEY } from '@/constants';
 
-interface ApiResult {
-  isSuccess: boolean;
+type ApiResult = ApiSuccess | ApiError;
+
+interface ApiSuccess {
+  isSuccess: true;
   result: string | undefined;
+}
+
+interface ApiError {
+  isSuccess: false;
+  errMsg?: string;
 }
 
 const configuration = new Configuration({
@@ -16,26 +23,28 @@ const openai = new OpenAIApi(configuration);
 export const createCompletion = async (message: string): Promise<ApiResult> => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `'${message}' 이라는 단어가 실제 쓰이는지 boolean 값으로 말해줘.`,
-    max_tokens: 4096,
-    temperature: 0,
+    prompt: `${message} is existed in Korean? and it is Noun? tell me if it is True, or False`,
+    max_tokens: 2048,
+    temperature: 0.2,
   });
   if (response.data?.choices) {
     const isExist = response.data.choices[0].text?.trim() === 'True';
-    return { isSuccess: isExist, result: message };
+    if (isExist) return { isSuccess: true, result: undefined };
   }
-  return { isSuccess: false, result: undefined };
+  return {
+    isSuccess: false,
+    errMsg: 'Api 요청을 불러오는 과정에서 문제가 발생했습니다.',
+  };
 };
 
 export const getNextWord = async (firstWord: string): Promise<ApiResult> => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `'${firstWord}' 로 시작하는 명사 단어를 하나 말해줘.`,
-    max_tokens: 4096,
-    temperature: 0,
+    prompt: `tell me one korean noun by starting '${firstWord}'. it's length is more than 2 and less than 4.`,
+    temperature: 0.2,
   });
   if (response.data?.choices) {
     return { isSuccess: true, result: response.data.choices[0].text?.trim() };
   }
-  return { isSuccess: false, result: undefined };
+  return { isSuccess: false };
 };
