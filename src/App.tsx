0850cf1b7367df849f493c19b15ecbe6ea3b41cd
RookieAND <gwangin1999@naver.com>
2023-02-18 22:39:23 +0900

    feat: 두음법칙 적용 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createCompletion, getNextWord } from '@/apis/API';
 import { useState } from 'react';
+import { CheckDoEeum } from './util/CheckDoEeum';
 
 function App() {
   const [word, setWord] = useState('');
@@ -7,9 +8,20 @@ function App() {
 
   const handleWord = (event: React.ChangeEvent<HTMLInputElement>) => {
     const typedWord = event.target.value;
+    if (typedWord.length > 3) return;
     setWord(
       typedWord.replace(/[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F]/gi, ''),
     );
+  };
+
+  const EnterWord = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter' && word.length < 4) {
+      sendQuestion();
+    }
+  };
+
+  const applyDoEeum = () => {
+    setWord(CheckDoEeum(word));
   };
 
   const sendQuestion = async () => {
@@ -21,12 +33,13 @@ function App() {
       console.log('AI 에게 답변을 받아오는데 실패했습니다.');
       return;
     }
-    console.log(word.at(-1));
+    setUsedWord((prevWord) => new Set([...prevWord, word]));
     const newWord = await getNewWord(word.at(-1) as string);
     if (!newWord) {
       console.log('문제가 발생하였습니다.');
       return;
     }
+    setWord(newWord.at(-1) as string);
     setUsedWord((prevWord) => new Set([...prevWord, newWord]));
   };
 
@@ -47,8 +60,17 @@ function App() {
 
   return (
     <div className="App">
-      <input value={word} onChange={handleWord}></input>
-      <button onClick={sendQuestion}>Test Send Quiz To Open AI</button>
+      <input value={word} onKeyDown={EnterWord} onChange={handleWord}></input>
+      <button onClick={sendQuestion}>단어 말하기</button>
+      <button onClick={applyDoEeum}>두음법칙 적용하기</button>
+      <h5>현재까지 이야기 한 단어 : {Array.from(usedWord).length}</h5>
+      <div>
+        {Array.from(usedWord).map((keyword: string, idx: number) => (
+          <p>
+            {idx % 2 == 0 ? '나' : 'AI'} : {keyword}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/util/CheckDoEeum.ts
@@ -0,0 +1,36 @@
+const HANGUL_FIRST_CODE = '가'.charCodeAt(0);
+const HANGUL_LAST_CODE = '힣'.charCodeAt(0);
+const JONGSEONG = 'Xㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ';
+
+export const CheckDoEeum = (word: string) => {
+  let char = word.charCodeAt(0);
+  if (char < HANGUL_FIRST_CODE || char > HANGUL_LAST_CODE) return word;
+  switch ((char - HANGUL_FIRST_CODE) / JONGSEONG.length) {
+    // 녀, 뇨, 뉴, 니 => 여, 요, 유, 니
+    case 48:
+    case 54:
+    case 59:
+    case 62:
+      char += 5292;
+      break;
+    // 랴, 려, 례, 료, 류, 리 => 야, 여, 예, 요, 유, 이
+    case 107:
+    case 111:
+    case 112:
+    case 117:
+    case 122:
+    case 125:
+      char += 3528;
+      break;
+    // 라, 래, 로, 뢰, 루, 르 => 아, 애, 오, 외, 우, 으
+    case 105:
+    case 106:
+    case 113:
+    case 116:
+    case 118:
+    case 123:
+      char -= 1764;
+      break;
+  }
+  return String.fromCharCode(char) + word.slice(1);
+};
